bd4e352061b019dd02035c3d1c1ca9c748d4f4aa
mikicvi <88291034+mikicvi@users.noreply.github.com>
2023-03-13 11:59:48 +0000

    Set up the subfolder and visual template

---
+++ java/src/c21737525/VilimsVisual.java
@@ -0,0 +1,40 @@
+package c21737525;
+
+import ie.tudublin.Visual;
+import ie.tudublin.VisualException;
+
+public class VilimsVisual extends Visual {
+
+    public void settings()
+    {
+        size(800, 800, P3D);
+        println("CWD: " + System.getProperty("user.dir"));
+        //fullScreen(P3D, SPAN);
+    }
+
+    public void keyPressed()
+    {
+        if (key == ' ')
+        {
+            getAudioPlayer().cue(0);
+            getAudioPlayer().play();
+            
+        }
+ 
+    }
+
+    public void setup()
+    {
+        colorMode(HSB);
+        noCursor();
+        
+        setFrameSize(256);
+
+        startMinim();
+        loadAudio("born_slippy.mp3");
+        getAudioPlayer().play();
+        //startListening(); 
+        
+    }
+    
+}
