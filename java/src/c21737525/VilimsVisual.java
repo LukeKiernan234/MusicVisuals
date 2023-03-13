790acb53037ffcd9f5feac1eadef3bbe1d010bec
mikicvi <88291034+mikicvi@users.noreply.github.com>
2023-03-13 13:06:59 +0000

    Playing with 3d objects and analysing how they interact

--- java/src/c21737525/VilimsVisual.java
+++ java/src/c21737525/VilimsVisual.java
@@ -37,6 +37,8 @@ public class VilimsVisual extends Visual {
         
     }
     
+    float angle1 = 0;
+    float angle2 = 0;
 
     public void draw()
     {
@@ -50,7 +52,40 @@ public class VilimsVisual extends Visual {
             e.printStackTrace();
         }
 
+        // Set background color
+        background(0);
         
+        
+        // Add some lights
+        ambientLight(50, 50, 50);
+        pointLight(255, 255, 255, width/2, height/2, -100);
+        
+        // Move the origin to the center of the window
+        translate(width/2, height/2);
+        
+        // Rotate the cube around Y axis
+        rotateY(angle1);
+        
+        // Draw a cube
+        fill(200,150,150);
+        box(100);
+        
+        // Move the origin along X axis by half of the cube size plus some offset
+        translate(100 + sin(angle2) *50 ,0 ,0 );
+        
+        // Rotate the sphere around Z axis
+        rotateZ(angle2);
+
+        // Draw a red sphere with radius 20
+        fill(255,255,255);
+        sphere(20);
+
+        // Update rotation angles
+        angle1 += radians(1);
+        angle2 += radians(5); 
+
+
+
 
 
     }
