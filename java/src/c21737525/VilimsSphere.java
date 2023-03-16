9876c80aeff0f94490979158cfcd01c79e356391
mikicvi <vilim.mikic@gmail.com>
2023-03-16 12:07:55 +0000

    Changing some comments. Removing some whitespace

--- java/src/c21737525/VilimsSphere.java
+++ java/src/c21737525/VilimsSphere.java
@@ -13,8 +13,8 @@ public class VilimsSphere extends Visual {
     }
 
     // This 3D visual is going to be a big sphere that is going to be rotating and the audio bands are going to be rotating around the sphere, like few moons around the planet.
-    // The sphere is going to be rotating around the center of the screen, and will be changing color based on bass.
-    // "Moons" are going to be rotating around the sphere, and the size of the "moon" is going to be based on the amplitude of the sound. They will be split into 6 groups, each group will be a different color.
+    // The sphere is going to be rotating around the center(using smoothed Amplitude) of the screen, and will be changing color based on smoothed amplitude.
+    // "Moons" are going to be rotating around the sphere, and the color of the "moon" is going to be based on the frequency bands. They will be split into 9 groups, each group will be a different color.
     // lerpedBuffer is going to be used to change the size of the "moon" based on the amplitude of the sound.
 
     /* IDEA: MAKE MOONS JUMP UP AND DOWN BASED ON THE AMPLITUDE, IT SHOULD BE LERPED. */
@@ -30,7 +30,7 @@ public class VilimsSphere extends Visual {
 
     
         gv.translate(gv.width / 2, gv.height / 2); // move the origin to the center of the screen
-        
+
         gv.stroke(255);
         gv.lights();
         gv.stroke(gv.map(gv.getSmoothedAmplitude(), 0, 1, 0, 255), 255, 255);
